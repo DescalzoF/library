c3f4eece0ff314e79a4c17c4a21f45e820699c95
Franco <fdescalzo@mail.austral.edu.ar>
2025-08-17 00:02:51 -0300

    created project and arquitecture. Needing to dockerize everything. Database already created and in theory dockerized

---
+++ src/App.tsx
@@ -0,0 +1,8 @@
+export default function App() {
+    return (
+        <div style={{ padding: 24 }}>
+            <h1>Home</h1>
+            <a href="/about">Go to About</a>
+        </div>
+    );
+}
