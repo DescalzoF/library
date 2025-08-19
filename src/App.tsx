edadfded9c27eddc4436560595375e3131e80658
Franco <fdescalzo@mail.austral.edu.ar>
2025-08-19 16:32:00 -0300

    currently finishing setting up project

--- src/App.tsx
+++
@@ -1,8 +0,0 @@
-export default function App() {
-    return (
-        <div style={{ padding: 24 }}>
-            <h1>Home</h1>
-            <a href="/about">Go to About</a>
-        </div>
-    );
-}
